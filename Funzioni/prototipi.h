7d4198df7c912f63c85d88ecd5630a154ba4398d
thisismichele <102221484+Micheles2102@users.noreply.github.com>
2024-12-18 13:38:41 +0100

    Create prototipi.h

---
+++ Funzioni/prototipi.h
@@ -0,0 +1,26 @@
+// File: prototipi.h
+// Questo file contiene tutti i prototipi delle funzioni
+
+#ifndef PROTOTIPI_H
+#define PROTOTIPI_H
+
+// Prototipo per la somma di due numeri interi
+int somma(int a, int b);
+
+// Prototipo per il prodotto di due numeri interi
+int prodotto(int a, int b);
+
+// Prototipo per calcolare l'area di un quadrato
+float area_quadrato(float lato);
+
+// Prototipo per calcolare l'area di un triangolo
+float area_triangolo(float base, float altezza);
+
+#endif // PROTOTIPI_H
+
+
+/*
+   Per utilizzare questi prototipi:
+   - Includi questo file nei tuoi sorgenti con #include "prototipi.h"
+   - Compila insieme ai file con le definizioni delle funzioni
+*/
